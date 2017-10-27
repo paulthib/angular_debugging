ac4f506708e3482f5979fa722447ef7ab2d3d8ae
Paul Thibodeaux <pthibodeaux@syncroness.com>
2017-10-27 13:34:25 -0600

    debugging app

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  servers = [];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    alert("remove " + id);
+    const position = id ;
+    this.servers.splice(position, 1);
+  }
+}
